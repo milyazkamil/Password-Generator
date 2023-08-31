7c02cd581002569ee0198660c1957f373676c3ea
Milyaz Kamil <milyazkamil@gmail.com>
2023-08-31 21:01:39 +0300

    created a data array containing the checkbox's information

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const checkboxes = [
+  {
+    id: 'number',
+    text: 'Include Numbers',
+  },
+  {
+    id: 'symbol',
+    text: 'Include Symbols',
+  },
+  {
+    id: 'lowercase',
+    text: 'Include Lowercase Letters',
+  },
+  {
+    id: 'uppercase',
+    text: 'Include Uppercase Letters',
+  },
+];
